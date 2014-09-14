e31cf305a0f1e4901b25bfc563c8257ad6f2fdb9
Sundar <mail@devsundar.com>
2014-09-14 13:33:57 -0700

    Adding about using TDD

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -9,6 +9,13 @@ describe PagesController do
     end
   end
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
